2ef68966a881ea5568912bb0f38f4491403bb7e3
ozlemonder <44654016+ozlemonder@users.noreply.github.com>
2018-11-01 21:07:29 +0300

    c

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main() {
-	int i;
+/*
+int i;
 	int notlar[5];
 	for(i=1;i<=5;i++){
 		printf("%d. dersin notunu giriniz: ",i);
@@ -12,4 +11,36 @@ int main() {
 	for(i=1;i<=5;i++){
 		printf("%d. not: %d\n",i,notlar[i]);
 	}
+*/
+
+int main() {
+	int a,b;
+	do{
+		printf("1. sayiyi girin: ");
+		scanf("%d",&a);
+		printf("2. sayiyi girin: ");
+		scanf("%d",&b);	
+		if(a<b)printf("Lutfen ilk sayiyi buyuk yazin \n");
+	}while(a<b);
+	
+	printf("%f",cikart(a,b));
 }
+
+int topla(int haydar, int tilki){
+	return haydar+tilki;
+}
+int cikart(int x,int y){
+	return x-y;
+}
+int bol(){
+	
+}
+
+
+
+
+
+
+
+
+
